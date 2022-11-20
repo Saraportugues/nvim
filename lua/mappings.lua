9cfaa341dfdd72da0351c54883ee2cfea25b8bea
MrQuatrelle <maigasmendo@gmail.com>
2022-11-20 12:26:06 +0000

    moving <C-w><S-*> keys to my cursor movement keys

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -18,6 +18,10 @@ keymap.set({"n"}, "<C-w>j", "<C-w>h")
 keymap.set({"n"}, "<C-w>k", "<C-w>j")
 keymap.set({"n"}, "<C-w>l", "<C-w>k")
 keymap.set({"n"}, "<C-w>ç", "<C-w>l")
+keymap.set({"n"}, "<C-w>J", "<C-w>H")
+keymap.set({"n"}, "<C-w>K", "<C-w>J")
+keymap.set({"n"}, "<C-w>L", "<C-w>K")
+keymap.set({"n"}, "<C-w>Ç", "<C-w>L")
 
 
 -- telescope
