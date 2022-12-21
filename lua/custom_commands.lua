145de3df6a23879c879cc18c275981727c702973
MrQuatrelle <maigasmendo@gmail.com>
2022-12-21 11:20:58 +0000

    added some more remaps

--- lua/custom_commands.lua
+++ lua/custom_commands.lua
@@ -29,8 +29,9 @@ api.nvim_create_autocmd({ "FileChangedShellPost" }, {
 })
 
 -- one less problem in this miserable world
-api.nvim_create_user_command("Wq", "wq", {})
-api.nvim_create_user_command("WQ", "wq", {})
 api.nvim_create_user_command("Q", "q", {})
 api.nvim_create_user_command("W", "w", {})
+api.nvim_create_user_command("Wq", "wq", {})
+api.nvim_create_user_command("WQ", "wq", {})
+api.nvim_create_user_command("Qa", "qa", {})
 
