a4cfcd69fcf329a284ec8b74353d832cc6d2a9de
MrQuatrelle <maigasmendo@gmail.com>
2022-12-31 12:16:10 +0000

    project pattern search keybind changed

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,7 +2,7 @@ local keymap = vim.keymap
 local builtin = require("telescope.builtin")
 
 keymap.set("n", "<M-e>", builtin.find_files, {})
-keymap.set("n", "<M-s>", function()
+keymap.set("n", "<leader>ps", function()
 	builtin.grep_string({
 		search = vim.fn.input(" Search > ")
 	})
