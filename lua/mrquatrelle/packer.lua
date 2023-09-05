9194e61122b1746706d2c4ddefb212d8894cabe2
MrQuatrelle <maigasmendo@gmail.com>
2023-09-05 21:49:07 +0100

    who uses '' anyways...

--- lua/mrquatrelle/packer.lua
+++ lua/mrquatrelle/packer.lua
@@ -29,30 +29,30 @@ return require("packer").startup(function(use)
         },
     }
     use {
-        'VonHeikemen/lsp-zero.nvim',
+        "VonHeikemen/lsp-zero.nvim",
         requires = {
             -- LSP Support
-            { 'neovim/nvim-lspconfig' },
-            { 'williamboman/mason.nvim' },
-            { 'williamboman/mason-lspconfig.nvim' },
+            { "neovim/nvim-lspconfig" },
+            { "williamboman/mason.nvim" },
+            { "williamboman/mason-lspconfig.nvim" },
 
             -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },
-            { 'hrsh7th/cmp-buffer' },
-            { 'hrsh7th/cmp-path' },
-            { 'saadparwaiz1/cmp_luasnip' },
-            { 'hrsh7th/cmp-nvim-lsp' },
-            { 'hrsh7th/cmp-nvim-lua' },
+            { "hrsh7th/nvim-cmp" },
+            { "hrsh7th/cmp-buffer" },
+            { "hrsh7th/cmp-path" },
+            { "saadparwaiz1/cmp_luasnip" },
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "hrsh7th/cmp-nvim-lua" },
 
             -- Snippets
-            { 'L3MON4D3/LuaSnip' },
-            { 'rafamadriz/friendly-snippets' },
+            { "L3MON4D3/LuaSnip" },
+            { "rafamadriz/friendly-snippets" },
         }
     }
     use {
-        'nvim-lualine/lualine.nvim',
+        "nvim-lualine/lualine.nvim",
         requires = {
-            'kyazdani42/nvim-web-devicons', opt = true
+            "kyazdani42/nvim-web-devicons", opt = true
         }
     }
     use {
