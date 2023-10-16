dfa6504c1618f37ba7f6ef17f76fbe42f571625e
MrQuatrelle <maigasmendo@gmail.com>
2023-10-16 10:07:20 +0100

    forgot I had neogen in my config... redoing keybinds

--- after/plugin/neogen.lua
+++ after/plugin/neogen.lua
@@ -1,16 +1,18 @@
+local vim = vim -- just so the lsp shuts up
+
 local neogen = require("neogen")
 neogen.setup({ snippet_engine = "luasnip" })
 
 local opts = { noremap = true, silent = true }
-vim.keymap.set("n", "<leader>nf", function()
-    neogen.generate({ type = 'func' })
+vim.keymap.set("n", "<leader>df", function()
+    neogen.generate({ type = "func" })
 end, opts)
-vim.keymap.set("n", "<leader>nc", function()
-    neogen.generate({ type = 'class' })
+vim.keymap.set("n", "<leader>dc", function()
+    neogen.generate({ type = "class" })
 end, opts)
-vim.keymap.set("n", "<leader>nt", function()
-    neogen.generate({ type = 'type' })
+vim.keymap.set("n", "<leader>dt", function()
+    neogen.generate({ type = "type" })
 end, opts)
-vim.keymap.set("n", "<leader>nh", function()
-    neogen.generate({ type = 'file' })
+vim.keymap.set("n", "<leader>dh", function()
+    neogen.generate({ type = "file" })
 end, opts)
