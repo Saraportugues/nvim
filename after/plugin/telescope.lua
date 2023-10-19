1845fec6b529118456995a6e34d0797227c7ebf3
MrQuatrelle <maigasmendo@gmail.com>
2023-10-19 17:00:47 +0100

    bunch of things... fixing bugs and updating code to match breaking changes on lsp-zero

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,14 @@
 local keymap = vim.keymap
+
+require("telescope").setup({
+    pickers = {
+        find_files = {
+            find_command = { "fd", "--type", "f", "--hidden", "--strip-cwd-prefix", "--exclude", ".git" },
+        },
+    }
+
+})
+
 local builtin = require("telescope.builtin")
 
 keymap.set("n", "<M-e>", builtin.find_files, {})
