5319eac56275cbfd19e59bb62e67abdf54a7d645
MrQuatrelle <maigasmendo@gmail.com>
2023-10-27 12:10:21 +0100

    making copilot not enable at start

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -1,5 +1,9 @@
+local vim = vim -- so the ls doesn't complain
+
 vim.g.copilot_no_tab_map = true
 vim.api.nvim_set_keymap("i", "<C-CR>", "copilot#Accept('<CR>')", {
     silent = true,
     expr = true
 })
+
+vim.cmd("Copilot disable")
